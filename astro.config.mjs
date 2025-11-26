8abc12b01e09f877b4bcb02258e32595fb3f50fd
Boris Radulov <boris@cbt.bg>
2025-11-26 18:51:14 +0200

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: cloudflare()
 });
